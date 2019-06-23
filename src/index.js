4ca7beb5daeb842bbbc22d21ca881bfc06381409
Robin Wieruch <wrobin@gmx.net>
2019-06-23 13:11:39 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import sum from './otherFile.js';
+
+console.log(sum(2, 5));
+
+document.querySelector('#app').innerHTML = 'Hello from your JavaScript file ...';
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hello Webpack bundled JavaScript Project',
+      template: 'src/index.html'
+    })
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
