3060d1ec052d9479d4dc755e2f14369d09837b49
Robin Wieruch <wrobin@gmx.net>
2019-06-24 19:23:37 +0200

    lodash as example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import { map } from 'lodash';
+
 import sum from './otherFile.js';
 
 console.log(sum(2, 5));
+
+console.log(map([1, 2], i => i + 1));
 
 document.querySelector('#app').innerHTML = 'Hello from your JavaScript file ...';
 
